2c362cde464971a559e1ccab714c3454a0c8547e
wagner-austin <austinwagner@msn.com>
2025-06-04 17:15:57 -0700

    Build: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   base: process.env.BASE_PATH ?? '/',
   build: { outDir: 'docs' },
 });
